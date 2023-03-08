f64f9de5b4af53c040ede87cd1ff91e617ade31c
Dimannn <diman1992nn@gmail.com>
2023-03-08 22:17:53 +0300

    add rightText1

--- src/test/java/ru/netology/PosrmanEchoTests.java
+++ src/test/java/ru/netology/PosrmanEchoTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-
 class PostmanEchoTests {
     @Test
     void shouldShowRightText() {
